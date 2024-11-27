6ed825568e5b538151cd07de696be9fec69c87e5
maria <maria.i.ferreira10@aluno.senai.br>
2024-11-27 10:51:52 -0500

    inicio do codigo

---
+++ mediaaluno.py
@@ -0,0 +1,8 @@
+nomes = int(input("numero de lunos"))
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+nota4 = float(input())
+
+faltas = int(input("Numero de faltas:10"))
+media = (nota1 + nota2 + nota3 + nota4) /4
