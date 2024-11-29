642524c81de4b70cefcc2dc22a77e4f8226ba882
Your Name <you@example.com>
2024-11-29 11:16:51 -0500

    continuação do codigo

--- mediaaluno.py
+++ mediaaluno.py
@@ -1,8 +1,38 @@
-nomes = int(input("numero de lunos"))
-nota1 = float(input())
-nota2 = float(input())
-nota3 = float(input())
-nota4 = float(input())
-
-faltas = int(input("Numero de faltas:10"))
-media = (nota1 + nota2 + nota3 + nota4) /4
+def calcularmedia(notas): #calcular a média dos aluno
+    return sum(notas) / len(notas) #receber uma lista de notas e retorna a média,
+def situacaoaluno(media,faltas) :
+    if faltas > 20: #se o aluno tiver mais de 20 faltas ele será reprovado
+        return "Reprovado pro falta"
+    elif media >= 7:
+        return "Aprovado"
+    elif media >= 5:
+        return "Recuperação" 
+    else:
+        return "Reprovado"
+def gerarrelatorio (num_alunos):
+    alunos =[]
+    for i in range(num_alunos):
+        print(f"\nAluno {i+1}:")
+        nome = input("Digite o nome do aluno: ")
+        faltas = int(input("Digite o número de faltas: "))
+        notas = []
+        for j in range(4):
+            nota = float(input(f"Digite a nota do {j+1}º bimestre:"))
+            notas.append(nota)
+            media = calcularmedia(notas)
+            situacao = situacaoaluno(media,faltas)
+            aluno = { 
+                 "nome": nome,
+                 "faltas": faltas,
+                 "notas": notas,
+                 "media": media,
+                 "situacao": situacao,
+             }
+            alunos.append(aluno)
+        print("\nRelatório final:")
+        print(f"{'nome'} :<20 {'faltas' :<7} {'media' :<6} {'situacao'}")
+        print("-" * 50)
+        for aluno in alunos:
+            print(f"{aluno['nome']:<20} {aluno['faltas']:<7} {aluno['media']:<6.2f} {aluno['situacao']}")
+def main():
+    
