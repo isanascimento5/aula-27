38d931a59bbba06cc417a1c21c7b56ef2ffcdc7b
maria <maria.i.ferreira10@aluno.senai.br>
2024-12-04 10:10:26 -0500

    alteração do dia 04.12.24

--- revisao.py
+++ revisao.py
@@ -1,14 +1,16 @@
 #ler entradas do usuário
-cont = 0 #variavel que controla a repetição
-escolha_usuario = int(input()) #variavel que guarda quantas vezes o codigo vai rodar
+alunos = []
+cont = 0
+escolha_usuario = int(input("Deseja calcular a média de quantos alunos?")) #variavel que guarda quantas vezes o codigo vai rodar
 while cont < escolha_usuario:
-    nome = (input()) #ARMAZENAR o nome do aluno
-    nota1 = float(input()) #4 notas do aluno
-    nota2 = float(input())
-    nota3 = float(input())
-    nota4 = float(input())
+    nome = (input("Digite o nome do aluno")) #ARMAZENAR o nome do aluno
+    
+    nota1 = float(input("Digite a nota 1:")) #4 notas do aluno
+    nota2 = float(input("Digite a nota 2:"))
+    nota3 = float(input("Digite a nota 3:"))
+    nota4 = float(input("Digite a nota 4:"))
 
-    faltas = int(input())
+    faltas = int(input("Digite a quantidade das faltas:"))
     #calculo da média
     media = (nota1+nota2+nota3+nota4)/4
 
@@ -18,18 +20,20 @@ while cont < escolha_usuario:
     elif media >= 8:
         situacao = "Aprovado"
     elif media >= 5: #Recuperação
-        recuperacao = float (input()) #Ler a nota da prova de recuperação
+        recuperacao = float (input("Digite a nota recuperacao")) #Ler a nota da prova de recuperação
         if recuperacao >=(10-media):
             situacao = "Aprovado na recuperação"
         else:
             situacao = "Reprovado na recuperação "
     else:
         situacao = "Reprovado por média"
-
+        #enviar os dados do aluno para a lista alunos 
+    alunos.append([nome , faltas , media , situacao])
         #Relatorio
-        print("Nome:", nome)
-        print("Notas:", nota1, nota2, nota3, nota4)
-        print("Faltas:", faltas)
-        print("Média:", media)
-        print("Situação:", situacao)
-        cont = cont + 1
+       # print("Nome:", nome)
+       # print("Notas:", nota1, nota2, nota3, nota4)
+        #print("Faltas:", faltas)
+        #print("Média:", media)
+        #print("Situação:", situacao)
+    cont = cont + 1
+print(alunos)
