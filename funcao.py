ef82c3168a7a36f28782b71fcdc72b590918e5ab
maria <maria.i.ferreira10@aluno.senai.br>
2024-12-11 08:47:25 -0500

    funcao

---
+++ funcao.py
@@ -0,0 +1,13 @@
+##DECLARAR UMA FUNÇÃO
+def bom_dia():
+    print("Bom dia!")
+
+def boa_noite():
+    print("Boa_noite!")
+
+def boa_tarde ():
+    print("Boa tarde!")
+#CHAMANDO AS FUNÇÕES
+bom_dia()
+boa_noite()
+boa_tarde()
