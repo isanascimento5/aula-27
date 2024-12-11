cd559e1a6fd36734e716fbb2fbb3ff15ac66b043
maria <maria.i.ferreira10@aluno.senai.br>
2024-12-11 09:59:23 -0500

    funcao2

--- funcao.py
+++ funcao.py
@@ -1,13 +1,15 @@
 ##DECLARAR UMA FUNÇÃO
-def bom_dia():
-    print("Bom dia!")
+def saudacoes(Hora_do_Dia): #Exibir a saudação correspondente a hora do dia
+    #DAR BOM DIA
+    if (Hora_do_Dia >= 0) and (Hora_do_Dia <=12):
+        print("Bom Dia!")
+    elif (Hora_do_Dia >= 13) and (Hora_do_Dia <= 18):
+        print("Boa Tarde!")
+    elif (Hora_do_Dia >= 19) and (Hora_do_Dia <=23):
+        print("Boa Noite!")
 
-def boa_noite():
-    print("Boa_noite!")
-
-def boa_tarde ():
-    print("Boa tarde!")
-#CHAMANDO AS FUNÇÕES
-bom_dia()
-boa_noite()
-boa_tarde()
+#FORA DE FUNÇÃO
+#peço para o usuário dizer a hora atual
+resposta = int(input("Digite que horas são: \n"))
+#chamo a função passando para ela o parametro obrigatorio
+saudacoes(resposta)
